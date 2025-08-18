2c0466e2af6b45ec3fcb32e9f6e155f2048b1de1
raghav123patel <gurjjarraghav123@gmail.com>
2025-08-18 00:12:09 -0700

    Create typescriptnotes.js

---
+++ typescriptnotes.js
@@ -0,0 +1,32 @@
+PS C:\Users\Administrator\Desktop\Typescript> npm init -y
+Wrote to C:\Users\Administrator\Desktop\Typescript\package.json:
+
+{
+  "name": "typescript",
+  "version": "1.0.0",
+  "main": "index.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "keywords": [],
+  "author": "",
+  "license": "ISC",
+  "description": ""
+}
+
+
+
+PS C:\Users\Administrator\Desktop\Typescript> npm install --save-dev typescript ts-node
+
+added 20 packages, and audited 21 packages in 7s
+
+found 0 vulnerabilities
+PS C:\Users\Administrator\Desktop\Typescript> npx tsc --init
+
+Created a new tsconfig.json                                                                                             
+                                                                                                                     TS       
+You can learn more at https://aka.ms/tsconfig
+PS C:\Users\Administrator\Desktop\Typescript> npx tsc
+PS C:\Users\Administrator\Desktop\Typescript> node dist/index.js
+aditya
+PS C:\Users\Administrator\Desktop\Typescript> 
